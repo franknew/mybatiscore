c5fe3c798e30f885680785810641f09b65093cda
frank <liuxiao.a.boy@gmail.com>
2018-07-26 17:32:47 +0800

    create
    create project, there're errors in codes

---
+++ MyBatis.Common/Logging/IConfigurationSectionHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace MyBatis.Common.Logging
+{
+    public interface IConfigurationSectionHandler
+    {
+        object Create(object parent, object configContext, XmlNode section);
+    }
+}
